5fb16e44a9cfac2f57cc039d7b8d240b6d844607
farisalfatih <farisalfatih777@gmail.com>
2024-03-06 20:38:32 +0700

    Pengenalan Python dasar

---
+++ p4-mengenal-variabel/main.py
@@ -0,0 +1,24 @@
+pengertianVariabel = "Dalam pemrograman Python, variabel adalah suatu nama yang digunakan untuk menyimpan nilai. Variabel memungkinkan Anda menyimpan dan merujuk nilai tertentu dalam program Anda. Saat Anda membuat variabel, Anda sebenarnya mengalokasikan ruang dalam memori komputer untuk menyimpan nilai tersebut."
+
+demostrasiPenggunaanVariabel = "nama_variabel = nilai"
+
+keterangan = "1. nama_variabel adalah nama variabel yang Anda tentukan.\n2. nilai adalah nilai yang ingin Anda simpan dalam variabel tersebut."
+
+contoh = "umur = 25\ntinggi = 175.5"
+
+penjelasanContoh = "Dalam contoh di atas, umur, nama, dan tinggi adalah nama variabel, dan masing-masing menyimpan nilai berupa angka, string, dan bilangan desimal."
+
+aturan = "Variabel dalam Python dapat diubah nilainya selama program berjalan, dan Anda dapat menggunakan variabel untuk melakukan berbagai operasi dan manipulasi data. Variabel juga digunakan untuk menyimpan hasil dari ekspresi atau operasi aritmatika."
+
+contohPerubahanNilaiVariabel = "umur = 25\numur = umur + 1\nprint(umur)  # Output: 26"
+
+keteranganPerubahan = "Dalam contoh ini, nilai variabel umur diubah dengan menambahkan 1 ke nilai aslinya."
+
+print(pengertianVariabel, "\n")
+print(demostrasiPenggunaanVariabel)
+print(keterangan, "\n")
+print(contoh)
+print(penjelasanContoh, "\n")
+print(aturan)
+print(contohPerubahanNilaiVariabel, "\n")
+print(keteranganPerubahan)
